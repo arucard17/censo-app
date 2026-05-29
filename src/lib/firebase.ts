c2151219172d0f80083f80223298082c6a2343f1
arucard17 <cristianace@outlook.com>
2026-05-29 17:47:24 -0500

    build with data

--- src/lib/firebase.ts
+++ src/lib/firebase.ts
@@ -2,12 +2,12 @@ import { initializeApp } from 'firebase/app'
 import { getFirestore } from 'firebase/firestore'
 
 const firebaseConfig = {
-  apiKey: import.meta.env.VITE_FIREBASE_API_KEY,
-  authDomain: import.meta.env.VITE_FIREBASE_AUTH_DOMAIN,
-  projectId: import.meta.env.VITE_FIREBASE_PROJECT_ID,
-  storageBucket: import.meta.env.VITE_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: import.meta.env.VITE_FIREBASE_MESSAGING_SENDER_ID,
-  appId: import.meta.env.VITE_FIREBASE_APP_ID,
+  apiKey: "AIzaSyBPpFZqQR_j9xXowv23gbAg4dggVL7kvts",
+  authDomain: "vereda-sonsito-2.firebaseapp.com",
+  projectId: "vereda-sonsito-2",
+  storageBucket: "vereda-sonsito-2.firebasestorage.app",
+  messagingSenderId: "654476017786",
+  appId: "1:654476017786:web:657c9ff744678330ad7bc3",
 }
 
 const app = initializeApp(firebaseConfig)
